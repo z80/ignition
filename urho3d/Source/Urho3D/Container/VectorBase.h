0fc4c908b26331aadad90228cbc3ddb4a02a0a6b
z80 <bashkirov.sergey@gmail.com>
2019-09-05 23:00:30 -0700

    Added Urho3d

---
+++ urho3d/Source/Urho3D/Container/VectorBase.h
@@ -0,0 +1,71 @@
+//
+// Copyright (c) 2008-2019 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#ifdef URHO3D_IS_BUILDING
+#include "Urho3D.h"
+#else
+#include <Urho3D/Urho3D.h>
+#endif
+
+#include "../Base/Iter.h"
+#include "../Container/Swap.h"
+
+namespace Urho3D
+{
+
+/// %Vector base class.
+/** Note that to prevent extra memory use due to vtable pointer, %VectorBase intentionally does not declare a virtual destructor
+    and therefore %VectorBase pointers should never be used.
+  */
+class URHO3D_API VectorBase
+{
+public:
+    /// Construct.
+    VectorBase() noexcept :
+        size_(0),
+        capacity_(0),
+        buffer_(nullptr)
+    {
+    }
+
+    /// Swap with another vector.
+    void Swap(VectorBase& rhs)
+    {
+        Urho3D::Swap(size_, rhs.size_);
+        Urho3D::Swap(capacity_, rhs.capacity_);
+        Urho3D::Swap(buffer_, rhs.buffer_);
+    }
+
+protected:
+    static unsigned char* AllocateBuffer(unsigned size);
+
+    /// Size of vector.
+    unsigned size_;
+    /// Buffer capacity.
+    unsigned capacity_;
+    /// Buffer.
+    unsigned char* buffer_;
+};
+
+}
