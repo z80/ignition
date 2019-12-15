44fc31abfd92ca3293be2ccceae6ec7dcb3694fc
z80 <bashkirov.sergey@gmail.com>
2019-12-14 21:42:28 -0800

    a

---
+++ urho3d/Source/Samples/00_ignition/relative_pose/src/ui/main_menu.cpp
@@ -0,0 +1,106 @@
+
+#include "main_menu.h"
+#include "environment.h"
+
+namespace Ign
+{
+
+MainMenu::MainMenu( Context * context )
+  : LogicComponent( context )
+{
+}
+
+MainMenu::~MainMenu()
+{
+}
+
+void MainMenu::Start()
+{
+    SubscribeToEvents();
+}
+
+void MainMenu::SubscribeToEvents()
+{
+    auto * cache = GetSubsystem<ResourceCache>();
+    auto * ui = GetSubsystem<UI>();
+
+    // Set up global UI style into the root UI element
+    auto * style = cache->GetResource<XMLFile>("UI/DefaultStyle.xml");
+    ui->GetRoot()->SetDefaultStyle(style);
+
+    // Create a Cursor UI element because we want to be able to hide and show it at will. When hidden, the mouse cursor will
+    // control the camera, and when visible, it will interact with the UI
+    SharedPtr<Cursor> cursor(new Cursor(context_));
+    cursor->SetStyleAuto();
+    ui->SetCursor(cursor);
+    // Set starting position of the cursor at the rendering window center
+    auto * graphics = GetSubsystem<Graphics>();
+    cursor->SetPosition(graphics->GetWidth() / 2, graphics->GetHeight() / 2);
+
+    // Load UI content prepared in the editor and add to the UI hierarchy
+    mainWnd_ = ui->LoadLayout( cache->GetResource<XMLFile>( "Ign/UI/MainMenu.xml") );
+    ui->GetRoot()->AddChild( mainWnd_ );
+
+    // Subscribe to button actions (toggle scene lights when pressed then released)
+    auto * button = mainWnd_->GetChildStaticCast<Button>("NewGame", true);
+    if ( button )
+        SubscribeToEvent( button, E_RELEASED, URHO3D_HANDLER( MainMenu, HandleNewGame ) );
+
+    button = mainWnd_->GetChildStaticCast<Button>( "Connect", true );
+    if ( button )
+        SubscribeToEvent( button, E_RELEASED, URHO3D_HANDLER( MainMenu, HandleConnect ) );
+
+    button = mainWnd_->GetChildStaticCast<Button>( "Settings", true );
+    if ( button )
+        SubscribeToEvent( button, E_RELEASED, URHO3D_HANDLER( MainMenu, HandleSettings ) );
+
+    button = mainWnd_->GetChildStaticCast<Button>( "Exit", true );
+    if ( button )
+        SubscribeToEvent( button, E_RELEASED, URHO3D_HANDLER( MainMenu, HandleExit ) );
+}
+
+void MainMenu::HandleNewGame( StringHash event, VariantMap & args )
+{
+    Scene * s = GetScene();
+    if ( !s )
+        return;
+
+    auto * e = s->GetComponent<Environment>();
+    if ( !e )
+        return;
+
+    e->StartServer();
+}
+
+void MainMenu::HandleConnect( StringHash event, VariantMap & args )
+{
+    Scene * s = GetScene();
+    if ( !s )
+        return;
+
+    auto * e = s->GetComponent<Environment>();
+    if ( !e )
+        return;
+
+    ClientDesc c;
+    c.login_ = "login";
+    c.password_ = "password";
+    e->Connect( c );
+}
+
+void MainMenu::HandleSettings( StringHash event, VariantMap & args )
+{
+}
+
+void MainMenu::HandleExit( StringHash event, VariantMap & args )
+{
+    //engine_->Exit();
+}
+
+
+}
+
+
+
+
+
---
+++ urho3d/Source/Samples/00_ignition/relative_pose/src/ui/main_menu.h
@@ -0,0 +1,42 @@
+
+#ifndef __MAIN_MENU_H_
+#define __MAIN_MENU_H_
+
+#include "Urho3D/Urho3DAll.h"
+
+namespace Ign
+{
+
+class MainMenu: public LogicComponent
+{
+    URHO3D_OBJECT( MainMenu, LogicComponent )
+public:
+    MainMenu( Context * context );
+    ~MainMenu();
+
+    void Start() override;
+
+protected:
+    void SubscribeToEvents();
+
+    // Event processing routines.
+    void HandleNewGame( StringHash event, VariantMap & args );
+    void HandleConnect( StringHash event, VariantMap & args );
+    void HandleSettings( StringHash event, VariantMap & args );
+    void HandleExit( StringHash event, VariantMap & args );
+
+
+    SharedPtr<UIElement> mainWnd_;
+};
+
+
+}
+
+
+
+
+#endif
+
+
+
+
