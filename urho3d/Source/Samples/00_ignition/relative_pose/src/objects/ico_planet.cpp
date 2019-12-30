a41f1886ba2bf28865cf99b4a706bf86ca3708aa
z80 <bashkirov.sergey@gmail.com>
2019-12-29 19:16:11 -0800

    a

--- urho3d/Source/Samples/00_ignition/relative_pose/src/objects/ico_planet.cpp
+++ urho3d/Source/Samples/00_ignition/relative_pose/src/objects/ico_planet.cpp
@@ -2,6 +2,11 @@
 #include "ico_planet.h"
 #include "settings.h"
 
+namespace Ign
+{
+
+const Float IcoPlanet::R_ = 10.0;
+
 void IcoPlanet::RegisterComponent( Context * context )
 {
     context->RegisterFactory<IcoPlanet>();
@@ -19,6 +24,23 @@ IcoPlanet::~IcoPlanet()
 
 }
 
+Float IcoPlanet::distance( RefFrame * refFrame ) const
+{
+    Vector3d rel_r;
+    Quaterniond rel_q;
+    relativePose( refFrame, rel_r, rel_q );
+    const Float d = rel_r.Length();
+    const Float dist = ( d > R_ ) ? (d - R_) : 0.0;
+    return dist;
+}
+
+Float IcoPlanet::distance( const Vector3d & r ) const
+{
+    const Float d = r.Length();
+    const Float dist = ( d > R_ ) ? (d - R_) : 0.0;
+    return dist;
+}
+
 void IcoPlanet::refStateChanged()
 {
     if ( !node_ )
@@ -27,7 +49,7 @@ void IcoPlanet::refStateChanged()
     const Vector3d    r0 = refR();
     const Quaterniond q = refQ();
 
-    Float scale  = 10.0;
+    Float scale  = R_;
     Float dScale = 0.7;
     const Float maxDist = Settings::staticObjDistanceHorizontShow();
     Vector3d r = r0;
@@ -63,9 +85,11 @@ void IcoPlanet::setupPhysicsContent( RigidBody2 * rb, CollisionShape2 * cs )
     ResourceCache * cache = GetSubsystem<ResourceCache>();
     Model * m = cache->GetResource<Model>("Ign/Models/IcoPlanet.mdl");
 
-    cs->SetGImpactMesh( m );
+    cs->SetGImpactMesh( m, R_ );
 }
 
 
+
+}
 
 
--- urho3d/Source/Samples/00_ignition/relative_pose/src/objects/ico_planet.h
+++ urho3d/Source/Samples/00_ignition/relative_pose/src/objects/ico_planet.h
@@ -16,11 +16,16 @@ public:
     IcoPlanet( Context * context );
     ~IcoPlanet();
 
+    Float distance( RefFrame * refFrame ) const override;
+    Float distance( const Vector3d & r=Vector3d::ZERO ) const override;
     void refStateChanged() override;
 
 protected:
     void createVisualContent( Node * n ) override;
     void setupPhysicsContent( RigidBody2 * rb, CollisionShape2 * cs ) override;
+
+private:
+    static const Float R_;
 };
 
 }
