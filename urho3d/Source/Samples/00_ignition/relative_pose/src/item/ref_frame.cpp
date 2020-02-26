f190d89b542c3686e085a4185c205cb8008cceab
testuser <testuser@sbashkirov-linux3>
2020-02-25 18:42:05 -0800

    a

--- urho3d/Source/Samples/00_ignition/relative_pose/src/item/ref_frame.cpp
+++ urho3d/Source/Samples/00_ignition/relative_pose/src/item/ref_frame.cpp
@@ -274,7 +274,7 @@ bool RefFrame::relativeState( const RefFrame * other, const State & stateInOther
         q_a = q_n_1 * q_a;
         if ( debugLogging )
         {
-            URHO3D_LOGINFOF( "Node %s", itemB->GetNode()->GetName().CString() );
+            URHO3D_LOGINFOF( "Node %s", itemA->name().CString() );
             URHO3D_LOGINFOF( "     r: (%f, %f, %f)", r_n_1.x_, r_n_1.y_, r_n_1.z_ );
             URHO3D_LOGINFOF( "     q: (%f, %f, %f, %f)", q_n_1.w_, q_n_1.x_, q_n_1.y_, q_n_1.z_ );
         }
@@ -300,7 +300,7 @@ bool RefFrame::relativeState( const RefFrame * other, const State & stateInOther
 
         if ( debugLogging )
         {
-            URHO3D_LOGINFOF( "Node %s", itemB->GetNode()->GetName().CString() );
+            URHO3D_LOGINFOF( "Node %s", itemB->name().CString() );
             URHO3D_LOGINFOF( "     r: (%f, %f, %f)", r_n_1.x_, r_n_1.y_, r_n_1.z_ );
             URHO3D_LOGINFOF( "     q: (%f, %f, %f, %f)", q_n_1.w_, q_n_1.x_, q_n_1.y_, q_n_1.z_ );
         }
