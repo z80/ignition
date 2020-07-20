34a116fe17799c997d122f9820a17244420a18ce
me <me@me.me>
2020-07-19 22:49:30 -0700

    a

---
+++ godot_projects/test_rf/mm/database.gd
@@ -0,0 +1,247 @@
+extends Node
+
+const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
+var db
+#var db_name = "user://test"
+var db_name := "res://data/test"
+#var db_name = "test"
+var json_name := "data/test_backup"
+var table_name := "company"
+var other_table_name := "expenses"
+
+var ids := [1,2,3,4,5,6,7]
+var names := ["Paul","Allen","Teddy","Mark","Robert","Julia","Amanda"]
+var ages := [32,25,23,25,30,63,13]
+var addresses := ["California","Texas","Baltimore","Richmond","Texas","Atlanta","New-York"]
+var salaries := [20000.00,15000.00,20000.00,65000.00,65000.00,65000.00,65000.00]
+
+var percentage_above_thirty := 0.05
+var percentage_below_thirty := 0.1
+var doomed_city := "Texas"
+
+func _ready():
+	example_of_basic_database_querying()
+	example_of_in_memory_and_foreign_key_support()
+	example_of_call_external_functions()
+
+# Basic example that goes over all the basic features available in the addon, such
+# as creating and dropping tables, inserting and deleting rows and doing more elementary
+# PRAGMA queries.
+func example_of_basic_database_querying():
+
+	# Make a big table containing the variable types.
+	var table_dict : Dictionary = Dictionary()
+	table_dict["id"] = {"data_type":"int", "primary_key": true, "not_null": true}
+	table_dict["name"] = {"data_type":"text", "not_null": true}
+	table_dict["age"] = {"data_type":"int", "not_null": true}
+	table_dict["address"] = {"data_type":"char(50)"}
+	table_dict["salary"] = {"data_type":"real"}
+
+	db = SQLite.new()
+	db.path = db_name
+	db.verbose_mode = true
+	# Open the database using the db_name found in the path variable
+	db.open_db()
+	# Throw away any table that was already present
+	db.drop_table(table_name)
+	# Create a table with the structure found in table_dict and add it to the database
+	db.create_table(table_name, table_dict)
+
+	var row_array : Array = []
+	var row_dict : Dictionary = Dictionary()
+	for i in range(0,ids.size()):
+		row_dict["id"] = ids[i]
+		row_dict["name"] = names[i]
+		row_dict["age"] = ages[i]
+		row_dict["address"] = addresses[i]
+		row_dict["salary"] = salaries[i]
+		row_array.append(row_dict.duplicate())
+
+		# Insert a new row in the table
+		db.insert_row(table_name, row_dict)
+		row_dict.clear()
+	#print(row_array)
+
+	# Select the id and age of the employees that are older than 30
+	var select_condition : String = "age > 30"
+	var selected_array : Array = db.select_rows(table_name, select_condition, ["id", "age"])
+	print("condition: " + select_condition)
+	print("result: ", selected_array)
+	
+	# Change name of 'Amanda' to 'Olga' and her age to 30
+	db.update_rows(table_name, "name = 'Amanda'", {"AGE":30, "NAME":"Olga"})
+
+	# Select the employee with the name Olga and with age 30
+	select_condition = "name = 'Olga' and age = 30"
+	selected_array = db.select_rows(table_name, select_condition, ["*"])
+	print("condition: " + select_condition)
+	print("result: ", selected_array)
+
+	# Delete the employee named Olga
+	db.delete_rows(table_name, "name = 'Olga'")
+
+	# Select all employees
+	select_condition = ""
+	selected_array = db.select_rows(table_name, select_condition, ["*"])
+	print("condition: " + select_condition)
+	print("result: ", selected_array)
+	# Check the types of the values in the dictionary
+	print("Types of selected columns:")
+	print("salary: ", typeof(selected_array[0]["salary"]))
+	print("age:    ", typeof(selected_array[0]["age"]))
+	print("name:   ", typeof(selected_array[0]["name"]))
+
+	# Delete all employees
+	db.delete_rows(table_name, "*")
+
+	# Add all employees again
+	db.insert_rows(table_name, row_array)
+
+	# Do a normal query
+	db.query("SELECT COUNT(*) AS 'number_of_employees' FROM " + table_name + ";")
+	print("There are ", db.query_result[0]["number_of_employees"], " employees in the company")
+
+	db.query("PRAGMA encoding;")
+	print("Current database encoding is: ", db.query_result[0]["encoding"])
+
+	# Export the table to a json-file with a specified name
+	db.export_to_json(json_name + "_new")
+
+	# Close the current database
+	db.close_db()
+
+	# Import (and, consequently, open) a database from an old backup json-file
+	print("Overwriting database content with old backup...")
+	db.import_from_json(json_name + "_old")
+
+	# Check which employees were present in this old json-file
+	select_condition = ""
+	selected_array = db.select_rows(table_name, select_condition, ["*"])
+	print("condition: " + select_condition)
+	print("result: ", selected_array)
+	# Check the types of the values in the dictionary
+	print("Types of selected columns:")
+	print("salary: ", typeof(selected_array[0]["salary"]))
+	print("age:    ", typeof(selected_array[0]["age"]))
+	print("name:   ", typeof(selected_array[0]["name"]))
+
+	# Import the data (in a destructive manner) from the new backup json-file
+	print("Overwriting database content again with latest backup...")
+	db.import_from_json(json_name + "_new")
+
+	# Try to delete a non-existant table from the database.
+	if not db.delete_rows(other_table_name, "*"):
+		print("SQL error: " + db.error_message)
+
+	# Close the imported database
+	db.close_db()
+
+# This example demonstrates the in-memory and foreign key support. It's
+# rather contrived, but it gets the point across.
+func example_of_in_memory_and_foreign_key_support():
+
+	# Create the database as usual.
+	db = SQLite.new()
+	# Enable in-memory storage.
+	db.path = ":memory:"
+	db.verbose_mode = true
+	# Enable foreign keys.
+	db.foreign_keys = true
+	# Open the database as usual.
+	db.open_db()
+
+	# Create a table for all your friends.
+	db.create_table("friends", {
+		"id": {"data_type": "int", "primary_key": true, "not_null": true},
+		"name": {"data_type": "text", "not_null": true, "unique": true},
+		"hobby": {"data_type": "int", "foreign_key": "hobbies.id", "not_null": true}
+	})
+
+	# Create a table for all your friends' hobbies.
+	db.create_table("hobbies", {
+		"id": {"data_type": "int", "primary_key": true, "not_null": true},
+		"description": {"data_type": "text", "not_null": true, "unique": true}
+	})
+
+	# ATTENTION: The important thing to note about the "friends" table is the
+	# definition of the foreign key "hobbies.id". This tells SQLITE to enforce
+	# the foreign key constraint, and that the field "friends.hobby" is now
+	# tied to the field "hobbies.id". Consequently, you are now required to
+	# specify a valid hobby when adding a friend to the database, which in
+	# turn means you first need to add some hobbies to the database before
+	# you can add any of your friends and assign them a hobby.
+	
+	# This won't work! There is no valid hobby with id 23 yet!
+	db.insert_rows("friends", [
+		{"id": 1, "name": "John", "hobby": 23}
+	])
+
+	# This will work! You create the hobby with id 23 first, then you can
+	# create your friend referencing that hobby.
+	db.insert_rows("hobbies", [
+		{"id": 23, "description": "Extreme Relaxing"}
+	])
+	db.insert_rows("friends", [
+		{"id": 1, "name": "John", "hobby": 23}
+	])
+
+	# Close the database.
+	db.close_db()
+
+func should_employee_be_fired(address : String) -> bool:
+	if address == doomed_city:
+		return true
+	else:
+		return false
+
+func increase_wages(salary : float, age : int) -> float:
+	if age > 30:
+		return (1.0 + percentage_above_thirty)*salary
+	else:
+		return (1.0 + percentage_below_thirty)*salary
+
+func example_of_call_external_functions():
+	# Make a big table containing the variable types.
+	var table_dict : Dictionary = Dictionary()
+	table_dict["id"] = {"data_type":"int", "primary_key": true, "not_null": true}
+	table_dict["name"] = {"data_type":"text", "not_null": true}
+	table_dict["age"] = {"data_type":"int", "not_null": true}
+	table_dict["address"] = {"data_type":"char(50)"}
+	table_dict["salary"] = {"data_type":"real"}
+
+	db = SQLite.new()
+	db.path = db_name
+	db.verbose_mode = true
+	# Open the database using the db_name found in the path variable
+	db.open_db()
+	# Throw away any table that was already present
+	db.drop_table(table_name)
+	# Create a table with the structure found in table_dict and add it to the database
+	db.create_table(table_name, table_dict)
+
+	var row_array : Array = []
+	var row_dict : Dictionary = Dictionary()
+	for i in range(0,ids.size()):
+		row_dict["id"] = ids[i]
+		row_dict["name"] = names[i]
+		row_dict["age"] = ages[i]
+		row_dict["address"] = addresses[i]
+		row_dict["salary"] = salaries[i]
+		row_array.append(row_dict.duplicate())
+
+		# Insert a new row in the table
+		db.insert_row(table_name, row_dict)
+		row_dict.clear()
+
+	var func_ref := funcref(self, "should_employee_be_fired")
+	db.create_function("should_employee_be_fired", func_ref, 1)
+
+	func_ref = funcref(self, "increase_wages")
+	db.create_function("increase_wages", func_ref, 2)
+
+	db.query("UPDATE company SET salary = increase_wages(salary, age);")
+	db.query("DELETE FROM company WHERE should_employee_be_fired(address);")
+
+	var select_condition := ""
+	var selected_array : Array = db.select_rows(table_name, select_condition, ["id", "salary", "name"])
+	print("result: ", selected_array)
--- godot_projects/test_rf/mm/motion_matching.gd
+++ godot_projects/test_rf/mm/motion_matching.gd
@@ -2,7 +2,8 @@ extends Node
 
 
 const SQLite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
-var DB_NAME = "res://mm_data.sqlite3.db"
+const FRAME_DB_NAME = "res://mm_data.sqlite3.db"
+const DESC_DB_NAME  = "res://desc_mm_data.sqlite3.db"
 var db_ = null
 const TABLE_DATA_NAME = "data"
 const TABLE_CONFIG_NAME = "config"
@@ -10,10 +11,22 @@ var frames_qty_: int = -1
 var frame_search_ = null
 var bone_names_ = null
 
+const ROOT_IND: int       = 0
+const LEFT_LEG_IND: int   = 1
+const RIGHT_LEG_IND: int  = 2
+const LEFT_HAND_IND: int  = 3
+const RIGHT_HAND_IND: int = 5
+const POSE_LIMB_INDS: Array = [ROOT_IND, LEFT_LEG_IND, RIGHT_LEG_IND, LEFT_HAND_IND, RIGHT_HAND_IND]
+const TRAJ_FRAME_INDS: Array = [30, 60, 90, 120]
+
+const FPS: float = 60.0
+const DT: float  = 1.0/FPS
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	open_database()
-	build_kd_tree()
+	var db = open_frame_database()
+	build_kd_tree( db )
 	
 	#for i in range( 3 ):
 	#	var f = frame_( i * 300 )
@@ -25,32 +38,89 @@ func _ready():
 #	pass
 
 
-func open_database():
-	db_ = SQLite.new()
-	db_.path = DB_NAME
-	var res: bool = db_.open_db()
+func open_frame_database():
+	var db = SQLite.new()
+	db.path = FRAME_DB_NAME
+	var res: bool = db.open_db()
 	
 	if not res:
-		print( "Failed to open frame database ", DB_NAME )
-		return false
+		print( "Failed to open frame database ", FRAME_DB_NAME )
+		return null
 		
-	db_.query("SELECT COUNT(*) AS 'qty' FROM " + TABLE_DATA_NAME + ";")
-	frames_qty_ = db_.query_result[0]['qty']
+	db.query("SELECT COUNT(*) AS 'qty' FROM " + TABLE_DATA_NAME + ";")
+	frames_qty_ = db.query_result[0]['qty']
 	
 	# Read bone names.
-	var selected_array: Array = db_.select_rows( TABLE_CONFIG_NAME, "id = \'names\'", ['data'] )
+	var selected_array: Array = db.select_rows( TABLE_CONFIG_NAME, "id = \'names\'", ['data'] )
 	if selected_array.size() < 1:
 		return false
 	var stri: String = selected_array[0]['data']
 	stri = stri.replace( "'", "\"" )
 	bone_names_ = JSON.parse( stri )
 	
+	return db
+
+
+func create_desc_database():
+	var db = SQLite.new()
+	db.path = DESC_DB_NAME
+	var res: bool = db_.open_db()
+	
+	if not res:
+		print( "Failed to open frame database ", DESC_DB_NAME )
+		return false
+	
+	# Create table for frame data and descriptors.
+	var table_dict : Dictionary = Dictionary()
+	table_dict["id"]   = {"data_type":"int", "primary_key": true, "not_null": true}
+	table_dict["pose"] = {"data_type":"text", "not_null": true}
+	table_dict["desc"] = {"data_type":"text", "not_null": true}
+	# Throw away any table that was already present
+	db.drop_table( 'frame' )
+	# Create a table with the structure found in table_dict and add it to the database
+	db.create_table( 'frame', table_dict )
+	
+	
+	# Create table for configuration.
+	# Currently bone names are stored here.
+	table_dict.clear()
+	table_dict["id"]   = {"data_type":"text", "primary_key": true, "not_null": true}
+	table_dict["data"] = {"data_type":"text", "not_null": true}
+	db.drop_table( 'config' )
+	# Create a table with the structure found in table_dict and add it to the database
+	db.create_table( 'config', table_dict )
+
 	return true
 
 
-func frame_( index: int ):
+func fill_desc_database( frame_db, desc_db ):
+	# I'm here now.....
+	pass
+
+
+
+func set_config_( db, key: String, data ):
+	var stri: String = JSON.print( data )
+	stri = stri.replace( "\"", "\'" )
+	var cmd: String = "INSERT OR REPLACE INTO config(id, data) VALUES (\'%s\', \'%s\');" % [key, stri]
+	var res: bool = db.query( cmd )
+	return res
+
+
+func get_config_( db, key: String ):
+	var cmd: String = "SELECT DISTINCT from config(data) WHERE id=\'%s\'" % key
+	db.query( cmd )
+	var stri: String = db.query_result[0]['data']
+	stri = stri.replace( "\'", "\"" )
+	var res = JSON.parse( stri )
+	res = res.result
+	return res
+
+
+
+func frame_( db, index: int ):
 	var select_condition: String = "id = %d" % index
-	var selected_array : Array = db_.select_rows('data', select_condition, ['data'])
+	var selected_array : Array = db.select_rows('data', select_condition, ['data'])
 	var stri = selected_array[0]['data']
 	stri = stri.replace( "'", "\"" )
 	#print( "raw data: ", stri )
@@ -65,12 +135,12 @@ func frame_( index: int ):
 	return res
 
 
-func build_kd_tree():
+func build_kd_tree( db ):
 	print( "Reading frames database..." )
 	frame_search_ = FrameSearch.new()
 	frame_search_.set_dims( 18 )
 	
-	var selected_array : Array = db_.select_rows('data', "", ['id', 'data'])
+	var selected_array : Array = db.select_rows('data', "", ['id', 'data'])
 	for v in selected_array:
 		var ind: int     = v['id']
 		var data: String = v['data']
@@ -88,6 +158,84 @@ func build_kd_tree():
 	print( "done" )
 
 
+func pose_desc_( db, index: int ):
+	var f = frame_( db, index )
+	var fp
+	if ( index > 0 ):
+		fp = frame_( db, index-1 )
+	else:
+		fp = f
+	
+	# Root pose
+	var root_q: Quat = Quat( f[ROOT_IND+1], f[ROOT_IND+2], f[ROOT_IND+3], f[ROOT_IND] )
+	var root_r: Vector3 = Vector3( f[ROOT_IND+4], f[ROOT_IND+5], f[ROOT_IND+6] )
+	var root_q_inv = root_q.inverse()
+	
+	var desc_r: Array
+	var desc_v: Array
+	for limb_ind in POSE_LIMB_INDS:
+		var ind: int = limb_ind * 7
+		var q: Quat = Quat( f[ind+1], f[ind+12], f[ind+13], f[ind+10] )
+		var r: Vector3 = Vector3( f[ind+4], f[ind+5], f[ind+6] )
+		#var qp: Quat = Quat( fp[ind+1], fp[ind+12], fp[ind+13], fp[ind+10] )
+		var rp: Vector3 = Vector3( fp[ind+4], fp[ind+5], fp[ind+6] )
+		r  -= root_r
+		rp -= root_r
+		r  = root_q_inv.xform( r )
+		rp = root_q_inv.xform( r )
+		#q  = root_q_inv * q
+		#qp = root_q_inv * qp
+		var v: Vector3 = (r - rp) * FPS
+		if ( limb_ind != ROOT_IND ):
+			desc_r += [ r.x, r.y ]
+		desc_v += [ v.x, v.y ]
+		
+	# Also compute Z in local ref. frame. It defines torso tilt.
+	var g: Vector3 = Vector3( 0.0, 0.0, 1.0 )
+	g = root_q_inv.xform( g )
+	var desc_g: Array = [ g.x, g.y ]
+		
+	return [ desc_r, desc_v, desc_g ]
 
+
+func traj_desc_( db, index: int ):
+	var f = frame_( db, index )
+	
+	# Root pose
+	var root_q: Quat = Quat( f[ROOT_IND+1], f[ROOT_IND+2], f[ROOT_IND+3], f[ROOT_IND] )
+	var root_r: Vector3 = Vector3( f[ROOT_IND+4], f[ROOT_IND+5], f[ROOT_IND+6] )
+	var root_q_inv = root_q.inverse()
+	
+	var desc_r: Array
+	var desc_az: Array
+	var desc_g: Array
+	
+	for ind in TRAJ_FRAME_INDS:
+		var frame_ind: int = index + ind
+		if frame_ind >= frames_qty_:
+			frame_ind = frames_qty_ - 1
+			var fn = frame_( db, frame_ind )
+			var q: Quat = Quat( fn[ROOT_IND+1], fn[ROOT_IND+2], fn[ROOT_IND+3], fn[ROOT_IND] )
+			var r: Vector3 = Vector3( fn[ROOT_IND+4], fn[ROOT_IND+5], fn[ROOT_IND+6] )
+			var q_inv: Quat = q.inverse()
+			r = r - root_r
+			r = root_q_inv.xform( r )
+			var d = [r.x, r.y]
+			desc_r += d
+			
+			var fwd: Vector3 = Vector3( 1.0, 0.0, 0.0 )
+			fwd = q.xform( fwd )
+			fwd = root_q_inv.xform( fwd )
+			d = [fwd.x, fwd.y]
+			desc_az += d
+			
+			var g: Vector3 = Vector3( 0.0, 0.0, 1.0 )
+			g =	q_inv.xform( g )
+			d = [g.x, g.y]
+			desc_g += d
+			
+	return [desc_r, desc_az, desc_g]
+		
+		
 
 
