b092cf85c28b00abf4fe9471fdbc8cb96676e170
me <me@me.me>
2020-08-01 16:40:58 -0700

    a

--- godot/modules/ignition/register_types.cpp
+++ godot/modules/ignition/register_types.cpp
@@ -2,15 +2,15 @@
 
 #include "core/class_db.h"
 #include "occupancy_grid.h"
-#include "ref_frame.h"
-#include "ref_frame_tree.h"
+//#include "ref_frame.h"
+//#include "ref_frame_tree.h"
 #include "ref_frame_node.h"
 
 void register_ignition_types()
 {
 	ClassDB::register_class<OccupancyGrid>();
-	ClassDB::register_class<RefFrame>();
-	ClassDB::register_class<RefFrameTree>();
+	//ClassDB::register_class<RefFrame>();
+	//ClassDB::register_class<RefFrameTree>();
 	ClassDB::register_class<RefFrameNode>();
 }
 
