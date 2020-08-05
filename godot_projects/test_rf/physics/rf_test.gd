9bce55ef5f633001e94aae0a6070d85a493ed9f2
me <me@me.me>
2020-08-05 13:44:47 -0700

    a

--- godot_projects/test_rf/physics/rf_test.gd
+++ godot_projects/test_rf/physics/rf_test.gd
@@ -6,6 +6,12 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var qa: Quat = Quat( 0.0, 0.0, 1.0/sqrt(2.0), 1.0/sqrt(2.0) )
+	var qb: Quat = Quat( 0.0, 1.0, 0.0, 0.0 )
+	var q = qa * qb
+	print( "IMU q: ", q )
+	
+	
 	var rf = $Rf
 	BodyCreator.root_node = self
 	rf.create_body( "plane" )
