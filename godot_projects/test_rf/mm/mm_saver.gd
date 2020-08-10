e7351dffcfb5fefe6bd0d48e1bf5a4d521adfd2b
me <me@me.me>
2020-08-10 14:12:55 -0700

    a

--- godot_projects/test_rf/mm/mm_saver.gd
+++ godot_projects/test_rf/mm/mm_saver.gd
@@ -13,6 +13,9 @@ var _mm_frame_ind: int = -1
 var _azimuth_initialized: bool = false
 var _inv_az_q: Quat = Quat.IDENTITY
 
+# Previous frame. This is for running interpolation.
+var _frame_prev = null
+
 # For determining "on ground" state.
 const LEFT_FOOT_IND:  int = 81
 const RIGHT_FOOT_IND: int = 86
@@ -26,6 +29,7 @@ func init():
 	_index = 0
 	_mm_frame_ind = -1
 	_azimuth_initialized = false
+	_frame_prev = null
 	_db = _open_database()
 
 
@@ -93,47 +97,74 @@ func store( mm, frame, frame_ind ):
 	if frame_ind == _mm_frame_ind:
 		return
 	
-	_init_azimuth( mm, frame )
+	# If there is no previous frame, make a deep copy and return as 
+	# Ther eis not previous frame for interpolation.
+	if _frame_prev == null:
+		# Make a deep copy and return.
+		var qty: int = frame.size()
+		_frame_prev = []
+		for i in range( qty ):
+			_frame_prev.push_back( frame[i] )
+		return
 	
-	_mm_frame_ind = frame_ind
+	# Two frames to interpolate in between.
+	var f0 = _frame_prev
+	var f1 = frame
 	
-	var data = {}
-	var gnd: Array = _compute_gnd( frame )
-	#print( "gnd: ", gnd )
+	var f: Array
 	
-	var q_r: Quat = _compute_azimuth( mm, frame )
-	data['q'] = [ q_r.w, q_r.x, q_r.y, q_r.z ]
-	
-	data['gnd'] = gnd
-	var links: Dictionary = {}
-	for ind in _bone_names.keys():
-		var v = _bone_names[ind]
-		var name: String = v["dest"]
-		var adj_q: Quat = v["q"]
-		var i = ind * 7
-		var x: float  = frame[i+4]
-		var y: float  = frame[i+5]
-		var z: float  = frame[i+6]
-		var qw: float = frame[i]
-		var qx: float = frame[i+1]
-		var qy: float = frame[i+2]
-		var qz: float = frame[i+3]
-		var q: Quat = Quat( qx, qy, qz, qw )
-		q = _inv_az_q * q * adj_q
-		var link: Dictionary = { 'x': x, 'y': y, 'z': z, 'qw': q.w, 'qx': q.x, 'qy': q.y, 'qz': q.z }
-		links[name] = link
-	data['links'] = links
-	var stri: String = JSON.print( data )
-	stri = stri.replace( "\"", "\'\'" )
-	
-	for i in range( FPS_MULTIPLIER ):
+	for repeat_ind in range( FPS_MULTIPLIER ):
+		var t: float
+		if FPS_MULTIPLIER > 1:
+			t = float(repeat_ind) / float(FPS_MULTIPLIER-1)
+		else:
+			t = 1.0
+		f = _slerp_frame( f0, f1, t )
+		
+		_init_azimuth( mm, f )
+		
+		_mm_frame_ind = frame_ind
+		
+		var data = {}
+		var gnd: Array = _compute_gnd( f )
+		#print( "gnd: ", gnd )
+		
+		var q_r: Quat = _compute_azimuth( mm, f )
+		data['q'] = [ q_r.w, q_r.x, q_r.y, q_r.z ]
+		
+		data['gnd'] = gnd
+		var links: Dictionary = {}
+		for ind in _bone_names.keys():
+			var v = _bone_names[ind]
+			var name: String = v["dest"]
+			var adj_q: Quat = v["q"]
+			var i = ind * 7
+			var x: float  = f[i+4]
+			var y: float  = f[i+5]
+			var z: float  = f[i+6]
+			var qw: float = f[i]
+			var qx: float = f[i+1]
+			var qy: float = f[i+2]
+			var qz: float = f[i+3]
+			var q: Quat = Quat( qx, qy, qz, qw )
+			q = _inv_az_q * q * adj_q
+			var link: Dictionary = { 'x': x, 'y': y, 'z': z, 'qw': q.w, 'qx': q.x, 'qy': q.y, 'qz': q.z }
+			links[name] = link
+		data['links'] = links
+		var stri: String = JSON.print( data )
+		stri = stri.replace( "\"", "\'\'" )
+		
 		var cmd_stri: String = "INSERT OR REPLACE INTO data(id, data) VALUES( %d, \'%s\' )" % [ _index, stri ]
 		var res: bool = _db.query( cmd_stri )
 		if not res:
 			print( "ERROR: failed to place frame information into the database" )
 			return
-		
+			
 		_index += 1
+	
+	# Update previous frame in order to 
+	# be able to smoothly interpolate.
+	_frame_prev = f
 
 
 
@@ -152,10 +183,58 @@ func _init_azimuth( mm, frame ):
 	var si2: float = sin(ang_2)
 	_inv_az_q = Quat( 0.0, 0.0, -si2, co2 )
 	_azimuth_initialized = true
-	
-	
 
 
+
+func _slerp_frame( f0: Array, f1: Array, t: float ):
+	var qty: int = f0.size()
+	var f = []
+	f.resize( qty )
+	for i in range(0, qty, 7):
+		var q0: Quat = Quat( f0[i+1], f0[i+2], f0[i+3], f0[i] )
+		var q1: Quat = Quat( f1[i+1], f1[i+2], f1[i+3], f1[i] )
+		var r0: Vector3 = Vector3( f0[i+4], f0[i+5], f0[i+6] )
+		var r1: Vector3 = Vector3( f1[i+4], f1[i+5], f1[i+6] )
+		var q: Quat    = _slerp_quat( q0, q1, t )
+		var r: Vector3 = _slerp_vector3( r0, r1, t )
+		f[i]   = q.w
+		f[i+1] = q.x
+		f[i+2] = q.y
+		f[i+3] = q.z
+		f[i+4] = r.x
+		f[i+5] = r.y
+		f[i+6] = r.z
+	
+	return f
+
+
+func _slerp_quat( q0: Quat, q1: Quat, t: float ):
+	var dot: float = q0.w*q1.w + q0.x*q1.x + q0.y*q1.y + q0.z*q1.z;
+	if dot < 0.0:
+		q1 = -q1
+		dot = -dot;
+	
+	# If almost the same quats interpolate linearly and 
+	# normalize.
+	if dot > 0.9995:
+		var q: Quat = q0*(1.0-t) + q1*t
+		q = q.normalized()
+		return q
+	
+	var theta_0: float     = acos(dot)
+	var theta: float       = theta_0 * t
+	var sin_theta: float   = sin(theta)
+	var sin_theta_0: float = sin(theta_0)
+	
+	var s0: float = cos(theta) - dot * sin_theta / sin_theta_0;
+	var s1: float = sin_theta / sin_theta_0;
+	var res: Quat = (q0 * s0) + (q1 * s1)
+	return res
+
+
+func _slerp_vector3( r0: Vector3, r1: Vector3, t: float ):
+	var r: Vector3 = r0*(1.0-t) + r1*t
+	return r
 
 
 
