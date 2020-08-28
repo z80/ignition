2e23caebe100c194af45268c6e81fa9124d6264a
me <me@me.me>
2020-08-27 18:38:33 -0700

    a

--- godot/modules/mm/openvr_capture/openvr_capture_node.cpp
+++ godot/modules/mm/openvr_capture/openvr_capture_node.cpp
@@ -1,4 +1,42 @@
 
 #include "openvr_capture_node.h"
 
+void OpenvrCaptureNode::_bind_methods()
+{
+	ClassDB::bind_method( D_METHOD("init", "int"), &OpenvrCaptureNode::init,    Variant::BOOL );
+	ClassDB::bind_method( D_METHOD("process"),     &OpenvrCaptureNode::process, Variant::BOOL );
+	ClassDB::bind_method( D_METHOD("pose", "int"), &OpenvrCaptureNode::pose,    Variant::TRANSFORM );
 
+}
+
+OpenvrCaptureNode::OpenvrCaptureNode()
+	: Node()
+{
+}
+
+OpenvrCaptureNode::~OpenvrCaptureNode()
+{
+}
+
+bool OpenvrCaptureNode::init( int qty )
+{
+	const bool res = _c.init( qty );
+	return res;
+}
+
+void OpenvrCaptureNode::finit()
+{
+	_c.finit();
+}
+
+bool OpenvrCaptureNode::process()
+{
+	const bool res = _c.process();
+	return res;
+}
+
+const Transform & OpenvrCaptureNode::pose( int i ) const
+{
+	const Transform & t = _c.pose( i );
+	return t;
+}
--- godot/modules/mm/register_types.cpp
+++ godot/modules/mm/register_types.cpp
@@ -3,10 +3,12 @@
 
 #include "core/class_db.h"
 #include "frame_search.h"
+#include "openvr_capture_node.h"
 
 void register_mm_types()
 {
 	ClassDB::register_class<MM::FrameSearch>();
+	ClassDB::register_class<OpenvrCaptureNode>();
 }
 
 void unregister_mm_types()
