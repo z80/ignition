fa246d6ed3ba05eacd2c95026fcf89239297ed38
me <me@me.me>
2020-10-17 20:00:17 -0700

    a

--- godot_projects/test_rf/learning/cache/anim_ctrl.gd
+++ godot_projects/test_rf/learning/cache/anim_ctrl.gd
@@ -26,7 +26,7 @@ const PARENTS: Array = [ 3, 3, 3, -1, 3, 3 ]
 const FPS: float = 24.0
 const DT: float  = 1.0/FPS
 const POSE_LIMB_INDS: Array = [TORSO_IND, LEFT_LEG_IND, RIGHT_LEG_IND]
-const TRAJ_TIME_MOMENTS: Array = [ 0.25, 0.50, 0.75, 1.00 ]
+const TRAJ_TIME_MOMENTS: Array = [ 0.25, 0.50, 0.75, 1.00, 1.25, 1.50 ]
 var TRAJ_FRAME_INDS: Array = []
 
 
