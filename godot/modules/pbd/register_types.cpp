35c5d27333c531e95fd71d63e00ce47cd74d8ccc
me <me@me.me>
2020-12-02 16:47:51 -0800

    a

--- godot/modules/pbd/register_types.cpp
+++ godot/modules/pbd/register_types.cpp
@@ -1,11 +1,29 @@
 
 #include "register_types.h"
+
+#include "core/engine.h"
 #include "core/class_db.h"
+
+#include "src/pbd_server.h"
+#include "src/_pbd_server.h"
+
+static PbdServer * pbd_server   = nullptr;
+static _PbdServer * _pbd_server = NULL;
 
 void register_pbd_types()
 {
+	pbd_server = memnew(PbdServer);
+	pbd_server->init();
+	_pbd_server = memnew(_PbdServer);
+	ClassDB::register_class<_PbdServer>();
+	Engine::get_singleton()->add_singleton(Engine::Singleton("PbdServer", _PbdServer::get_singleton()));
 }
 
 void unregister_pbd_types()
 {
+	if ( pbd_server )
+		memdelete( pbd_server );
+
+	if ( _pbd_server )
+		memdelete( _pbd_server );
 }
--- godot/modules/pbd/register_types.h
+++ godot/modules/pbd/register_types.h
@@ -1,6 +1,6 @@
 
-#ifndef __PBD_H_
-#define __PBD_H_
+#ifndef __REGISTER_PBD_TYPES_H_
+#define __REGISTER_PBD_TYPES_H_
 
 void register_pbd_types();
 void unregister_pbd_types();
