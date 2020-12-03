5c086b8af15e6270c8311df170dad6befdfa5743
me <me@me.me>
2020-12-02 17:58:13 -0800

    a

---
+++ godot/modules/pbd/src/pbd_simulation_node.h
@@ -0,0 +1,33 @@
+
+#ifndef __PBD_SIMULATION_NODE_H_
+#define __PBD_SIMULATION_NODE_H_
+
+#include "scene/3d/spatial.h"
+
+class PbdSimulationNode: public Spatial
+{
+	GDCLASS( PbdSimulationNode, Spatial );
+
+public:
+	PbdSimulationNode();
+	~PbdSimulationNode();
+
+protected:
+	void _notification( int p_what );
+	static void _bind_methods();
+
+public:
+	void step();
+
+	RID sim_rid;
+
+private:
+	bool init();
+	void finit();
+};
+
+
+
+#endif
+
+
