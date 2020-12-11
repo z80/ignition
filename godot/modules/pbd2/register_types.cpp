9e85e556f300c3b2cf15b12dd46a8bf67205d106
me <me@me.me>
2020-12-10 18:53:57 -0800

    a

--- godot/modules/pbd2/register_types.cpp
+++ godot/modules/pbd2/register_types.cpp
@@ -4,13 +4,25 @@
 #include "core/engine.h"
 #include "core/class_db.h"
 
+#include "pbd2_simulation_node.h"
+#include "pbd2_rigid_body_node.h"
+#include "pbd2_joint_node.h"
+#include "pbd2_joint_hinge_node.h"
+#include "pbd2_joint_ball_node.h"
+
 void register_pbd2_types()
 {
-    //ClassDB::register_class<Pbd2SimulationNode>();
-    //ClassDB::register_class<Pbd2RigidBodyNode>();
-    //ClassDB::register_class<Pbd2ContactPtNode>();
+    ClassDB::register_class<Pbd::PbdSimualionNode>();
+    ClassDB::register_class<Pbd::PbdRigidBodyNode>();
+    ClassDB::register_class<Pbd::PbdJointNode>();
+    ClassDB::register_class<Pbd::PbdJointHingeNode>();
+    ClassDB::register_class<Pbd::PbdJointBallNode>();
 }
 
 void unregister_pbd2_types()
 {
 }
+
+
+
+
