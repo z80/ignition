ad989eb0d806d1a8cf62666f23f8ca44b9e85431
me <me@me.me>
2020-12-13 16:14:14 -0800

    a

--- godot_projects/test_rf/physics/orbital_motion/hyperbolic.gd
+++ godot_projects/test_rf/physics/orbital_motion/hyperbolic.gd
@@ -3,52 +3,64 @@ const Consts = preload( "res://physics/orbital_motion/constants.gd" )
 const Velocity = preload( "res://physics/orbital_motion/velocity.gd" )
 
 static func init( r: Vector3, v: Vector3, args: Dictionary ):
+	r = args.inv_A.xform( r )
+	v = args.inv_A.xform( v )
+	
 	var a: float = args.a
 	var gm: float = args.gm
 	var abs_e: float = args.abs_e
 	# Semi-latus-rectum.
-	var slr: float = args.a * (1.0 - abs_e*abs_e)
+	var slr: float = a * (1.0 - abs_e*abs_e)
+	var n: float = sqrt( -a*a*a/gm )
 	
-	var abs_r: float = r.length()
-	var cos_E: float = 1.0 - abs_r/a
-	var r_dot_v: float = r.dot( v )
-	var sin_E: float = r_dot_v / ( abs_e * sqrt( gm * a ) )
-	var E: float = atan2( sin_E, cos_E )
+	# True anomaly from "r".
+	#var f: float = atan2( r.y, r.x )
+	# Hyperbolic eccentric anomaly E (or H).
+	var co_f: float = r.x/r.length()
+	var cosh_E: float = (co_f + abs_e)/(1.0 + abs_e*co_f)
+	var sinh_E: float = sqrt(cosh_E*cosh_E - 1.0)
+	if r.y < 0.0:
+		sinh_E = -sinh_E
+	var exp_E: float = cosh_E + sinh_E
+	var E: float = log( exp_E )
 	
-	var n: float = sqrt( a*a*a/gm )
-	# Orbital period.
-	var T: float = 2.0*PI*n
-	# Time to periapsis.
-	var periapsis_t: float = ( abs_e*sin(E) - E ) * n
+	var M: float = abs_e * sinh_E - E
+	var periapsis_t = M * n
 	
 	# Filling in parameters
 	args.slr = slr
 	args.E   = E
-	args.T   = T
+	args.n   = n
 	args.periapsis_t = periapsis_t
 
 
 static func process( dt: float, args: Dictionary ):
 	var periapsis_t: float = args.periapsis_t + dt
-	if periapsis_t > args.T:
-		periapsis_t -= args.T
+	args.periapsis_t = periapsis_t
 	
+	var n: float = args.n
 	var M: float = periapsis_t / args.n
 	var E: float = solve( args.abs_e, M, args.E )
 	
-	var co_E: float = cos( E )
-	var si_E: float = sin( E )
-	var x: float = args.a*(co_E - args.abs_e)
-	var y: float = args.a*(1.0 - args.abs_e*args.abs_e)*si_E
-	var r2: Vector2 = Vector2( x, y )
+	var coh_E: float = cosh( E )
+	var abs_e: float = args.abs_e
+	var co_f: float  = ( coh_E - abs_e )/( 1.0 - abs_e*coh_E )
+	var si_f: float = sqrt( 1.0 - co_f*co_f )
+	if E < 0.0:
+		si_f = -si_f
 	
-	var f: float = abs( acos( (args.abs_e - co_E)/(args.abs_e*co_E - 1.0) ) )
-	var v2: Vector2 = Velocity.velocity( args, f )
+	var abs_r = args.slr/(1.0 + abs_e*co_f)
+	var x: float = abs_r * co_f
+	var y: float = abs_r * si_f
+	var r: Vector3 = Vector3( x, y, 0.0 )
 	
-	var r3: Vector3 = (args.e_x * r2.x) + (args.e_y * r2.y)
-	var v3: Vector3 = (args.e_x * v2.x) + (args.e_y * v2.y)
+	var v: Vector3 = velocity( args, r )
 	
-	return [r3, v3]
+	var A: Basis = args.A
+	r = A.xform( r )
+	v = A.xform( v )
+	
+	return [r, v]
 
 
 static func solve( e: float, M: float, E: float ):
@@ -61,19 +73,37 @@ static func solve( e: float, M: float, E: float ):
 		ret = solve_next( e, M, En )
 		err = ret[0]
 		En  = ret[1]
-		if En > Consts._2PI:
-			En -= Consts._2PI
-		elif En < 0.0:
-			En += Consts._2PI
+		iters += 1
 	return En
 	
 
 
 static func solve_next( e: float, M: float, E: float ):
-	var si_E: float = sin( E )
-	var co_E: float = cos( E )
-	E = E - ( E - e*si_E - M ) / (1.0  - e*co_E)
-	si_E = sin( E )
-	var err: float = abs( E - e*si_E - M )
+	var sih_E: float = sinh( E )
+	var coh_E: float = cosh( E )
+	E = E - ( e*sih_E - E - M ) / (e*coh_E - 1.0)
+	sih_E = sinh( E )
+	var err: float = abs( e*sih_E - E - M )
 	return [err, E]
+
+
+
+
+static func velocity( args: Dictionary, r: Vector3 ):
+	var gm: float    = args.gm
+	var a: float     = args.a
+	var abs_e: float = args.abs_e
+	var abs_r: float = r.length()
+	
+	var si_f: float = r.x/abs_r
+	var co_f: float = r.y/abs_r
+	
+	var v: Vector3 = Vector3( -si_f, co_f + abs_e, 0.0 )
+	v = v.normalized()
+	var abs_v: float = sqrt( gm*( 2.0/abs_r - 1.0/a ) )
+	v *= abs_v
+	return v
+
+
+
 
--- godot_projects/test_rf/test/unit/test_orbital.gd
+++ godot_projects/test_rf/test/unit/test_orbital.gd
@@ -10,7 +10,7 @@ func after_all():
 	pass
 
 
-func test_back_forth():
+func test_back_forth_elliptic():
 	var gm: float = 1.0
 	
 	for i in range( 10 ):
@@ -26,6 +26,21 @@ func test_back_forth():
 		print( "vx: ", vx, ", vy: ", vy )
 		print( "v:  ", ret[1] )
 		print( "" )
+
+
+func test_back_forth_hyperbolic():
+	var gm: float = 1.0
 	
-	
-	
+	for i in range( 10 ):
+		var rx: float  = rand_range( -2.0, 2.0 )
+		var ry: float  = rand_range( -2.0, 2.0 ) * 0.0
+		
+		var vx: float  = rand_range(  -0.11, 0.11 )
+		var vy: float  = rand_range(  0.01, 1.0 ) + 3.0
+		var args: Dictionary = Motion.init( gm, Vector3( rx, ry, 0.0 ), Vector3( vx, vy, 0.0 ) )
+		var ret: Array = Motion.process( 0.0, args )
+		print( "rx0: ", rx, ", ry0: ", ry )
+		print( "r:  ", ret[0] )
+		print( "vx: ", vx, ", vy: ", vy )
+		print( "v:  ", ret[1] )
+		print( "" )
