c6eb292aab9e95e3557b28f29cc138edce854af8
me <me@me.me>
2020-12-30 23:44:34 -0800

    a

---
+++ godot/modules/ignition/sphere/height_source_gd_ref.h
@@ -0,0 +1,34 @@
+
+#ifndef __HEIGHT_SOURCE_TEST_REF_H_
+#define __HEIGHT_SOURCE_TEST_REF_H_
+
+#include "height_source_ref.h"
+#include "height_source_test.h"
+
+
+namespace Ign
+{
+
+class HeightSourceTestRef: public HeightSourceRef
+{
+	GDCLASS( HeightSourceTestRef, HeightSourceRef );
+	OBJ_CATEGORY("Ignition");
+
+public:
+	HeightSourceTestRef();
+	~HeightSourceTestRef();
+
+	HeightSourceTest height_source_test;
+};
+
+}
+
+
+
+
+#endif
+
+
+
+
+
