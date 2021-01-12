6a9ad0141fb4ce2ad55ac086e47fc14c7a9a09d8
me <me@me.me>
2021-01-12 15:11:32 -0800

    a

--- godot/modules/pbd2/src/pbd2_narrow_tree_node.h
+++ godot/modules/pbd2/src/pbd2_narrow_tree_node.h
@@ -20,7 +20,7 @@ public:
 	NarrowTreeNode( const NarrowTreeNode & inst );
 	const NarrowTreeNode & operator=( const NarrowTreeNode & inst );
 
-        void apply( const SE3 & se3 );
+    void apply( const SE3 & se3 );
 
 	bool hasChildren() const;
 	bool subdivide();
@@ -28,8 +28,8 @@ public:
 	// Queries.
 	// Intersection with a triangle. It is for initial fill up.
 	bool inside( const Face & face ) const;
-        // This is for recursive collision detection.
-        // Will need to extend it with "optimal" box later.
+    // This is for recursive collision detection.
+    // Will need to extend it with "optimal" box later.
 	bool inside( const NarrowTreeNode & n ) const;
 
 
@@ -37,12 +37,12 @@ public:
 	void init();
 
 
-        bool collide_forward( const NarrowTreeNode & n, Vector<Vector3d> & pts, Vector<Vector3d> & depths ) const;
-        bool collide_backward( const NarrowTreeNode & this_node, Vector<Vector3d> & pts, Vector<Vector3d> & depths ) const;
-        bool collide_faces( const NarrowTreeNode & this_node, Vector<Vector3d> & pts, Vector<Vector3d> & depths ) const;
+    bool collide_forward( const NarrowTreeNode & n, Vector<Vector3d> & pts, Vector<Vector3d> & depths ) const;
+    bool collide_backward( const NarrowTreeNode & this_node, Vector<Vector3d> & pts, Vector<Vector3d> & depths ) const;
+    bool collide_faces( const NarrowTreeNode & this_node, Vector<Vector3d> & pts, Vector<Vector3d> & depths ) const;
 
-        // Compute "se3_optimized_" and "cube_optimized_".
-        bool compute_cube_optimized();
+    // Compute "se3_optimized_" and "cube_optimized_".
+    bool compute_cube_optimized();
 
 	NarrowTree * tree;
 	int absIndex;
@@ -53,14 +53,14 @@ public:
 	// Child indices in Octtree list.
 	int children[8];
         
-        // Value is number of triangles inside.
+    // Value is number of triangles inside.
 	int      value;
 	Float    size2; // Size over 2.
 	Vector3d center;
         
-        Cube cube_;
-        SE3  se3_optimized_;
-        Cube cube_optimized_;
+    Cube cube_;
+    SE3  se3_optimized_;
+    Cube cube_optimized_;
 
 	Vector<int> ptInds;
 };
