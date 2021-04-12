c62bd416dc73e4ba111a03b780601ab37b37599c
me <me@me.me>
2021-04-11 22:01:47 -0700

    a

--- godot_projects/test_rf/physics/utils/closest_celestial_body.gd
+++ godot_projects/test_rf/physics/utils/closest_celestial_body.gd
@@ -12,7 +12,15 @@ static func _celestial_body_recursive( n: Node ):
 	
 	var cb: CelestialSurface = n as CelestialSurface
 	if cb != null:
-		return n
+		return cb
+	
+	# Try the owner (root of local scene 
+	# created during scene design).
+	var o: Node = n.owner
+	if o != null:
+		cb = o as CelestialSurface
+		if cb != null:
+			return cb
 	
 	var p: Node = n.get_parent()
 	return _celestial_body_recursive( p )
