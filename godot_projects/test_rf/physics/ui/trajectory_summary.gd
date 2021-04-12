00c779dc29d8a6c7e5cedad644c117ba15f59987
me <me@me.me>
2021-04-11 23:00:11 -0700

    a

--- godot_projects/test_rf/physics/ui/trajectory_summary.gd
+++ godot_projects/test_rf/physics/ui/trajectory_summary.gd
@@ -4,7 +4,7 @@ extends Panel
 
 var _cm: CelestialMotionRef = null
 var _do_show: bool = false
-
+var _target_name: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,9 +22,18 @@ func _on_Timer_timeout():
 
 
 func _update_celestial_motion():
-	var ctrl = PhysicsManager.player_control as Body
+	var ctrl: Body = PhysicsManager.player_control as Body
 	if ctrl == null:
 		_do_show = false
+		return
+	var ClosestCelestialBody = preload( "res://physics/utils/closest_celestial_body.gd" )
+	var p: Node = self.get_parent()
+	var cb: CelestialSurface = ClosestCelestialBody.closest_celestial_body( p )
+	if cb == null:
+		_do_show = false
+	var se3: Se3Ref = ctrl.relative_to( cb )
+	_cm.init( cb.gm, se3 )
+	_target_name = cb.name
 	_do_show = true
 	
 
@@ -36,7 +45,27 @@ func _visualize():
 
 
 func _visualize_celestial_motion():
-	pass
+	var l: Label = get_node( "Target" )
+	l.text = _target_name
+	l = get_node( "TrajectoryType" )
+	l.text = _cm.movement_type()
+	l = get_node( "Apogee" )
+	l.text = str( _cm.apogee() * 0.001 )
+	l = get_node( "Perigee" )
+	l.text = str( _cm.perigee() * 0.001 )
+	l = get_node( "TimeToPerigee" )
+	l.text = "--:--"
+	l = get_node( "TimeToApogee" )
+	l.text = "--:--"
+	
+	l = get_node( "VPerigee" )
+	l.text = "--:--"
+	l = get_node( "VPerigee" )
+	l.text = "--:--"
+	l = get_node( "Period" )
+	l.text = "--:--"
+	l = get_node( "Velocity" )
+	l.text = "--:--"
 
 
 func _visualize_none():
