ea2d92622bfa8f22c95afc1700205f44c7e86cbd
me <me@me.me>
2021-05-27 17:54:55 -0700

    a

--- godot_projects/test_rf/physics/parts/fuel_tanks/fuel_tank.gd
+++ godot_projects/test_rf/physics/parts/fuel_tanks/fuel_tank.gd
@@ -2,9 +2,13 @@
 extends Part
 class_name FuelTank
 
+enum FuelType {LIQUID_FUEL=0, LIQUID_OXIDIZER=1, SOLID_FUEL=2}
+
 export(Resource) var fuel_params = preload( "res://physics/parts/fuel_tanks/resource_fuel_tanks.tres" )
 
+export(FuelType) var fuel_type = FuelType.LIQUID_FUEL
 export(float) var total_volume = 10.0
+
 var _initial_volume: float = 0.0
 var _fuel_left: float    = 0.0
 
@@ -12,6 +16,9 @@ var _fuel_left: float    = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	._ready()
+	
+	_fuel_left = _initial_volume
+
 
 
 func init():
---
+++ godot_projects/test_rf/physics/parts/fuel_tanks/resource_fiel_tanks.gd
@@ -0,0 +1,11 @@
+
+extends Resource
+
+export(float) var fuel_density     = 1.0
+export(float) var oxidizer_density = 2.0
+
+
+
+
+
+
