31e2a8723dacdc891734dee4fac10d2e37855308
me <me@me.me>
2021-05-28 18:08:48 -0700

    a

---
+++ godot_projects/test_rf/physics/parts/part_search.gd
@@ -0,0 +1,14 @@
+
+static func find_liquid_fuel_tanks( part: Part ):
+	var visited: Array = []
+	var found: Array = []
+	find_liquid_fuel_tanks_recursive( part, visited, found )
+	return found
+
+
+static func find_liquid_fuel_tanks_recursive( part: Part, visited: Array, found: Array ):
+	pass
+
+
+
+
