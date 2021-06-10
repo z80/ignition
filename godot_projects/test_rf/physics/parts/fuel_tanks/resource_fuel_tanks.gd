b6762571a362818ca4997b65b98c02bf982778fc
me <me@me.me>
2021-06-09 22:37:42 -0700

    a

--- godot_projects/test_rf/physics/parts/fuel_tanks/resource_fuel_tanks.gd
+++ godot_projects/test_rf/physics/parts/fuel_tanks/resource_fuel_tanks.gd
@@ -2,7 +2,7 @@
 extends Resource
 
 export(float) var liquid_fuel_density = 1.0
-export(float) var oxidizer_density    = 2.0
+export(float) var liquid_oxidizer_density = 2.0
 export(float) var solid_fuel_density  = 3.0
 # The volume difference when equalization occurs.
 export(float) var delta_volume = 1.0
--- godot_projects/test_rf/physics/parts/thrusters/thruster.gd
+++ godot_projects/test_rf/physics/parts/thrusters/thruster.gd
@@ -199,12 +199,14 @@ func _process_liquid_fuel( _delta: float ):
 	if fuel_left <= 0.0:
 		_liquid_fuel_tank._fuel_left = 0.0
 		_ignited = false
+	_liquid_fuel_tank._fuel_left = fuel_left
 	_liquid_fuel_tank._mass_changed = true
 	
 	fuel_left = _liquid_oxidizer_tank._fuel_left - dv
 	if fuel_left <= 0.0:
 		_liquid_oxidizer_tank._fuel_left = 0.0
 		_ignited = false
+	_liquid_oxidizer_tank._fuel_left = fuel_left
 	_liquid_oxidizer_tank._mass_changed = true
 
 
@@ -220,6 +222,7 @@ func _process_solid_fuel( _delta: float ):
 	if fuel_left <= 0.0:
 		_solid_fuel_tank._fuel_left = 0.0
 		_ignited = false
+	_solid_fuel_tank._fuel_left = fuel_left
 	_solid_fuel_tank._mass_changed = true
 
 
