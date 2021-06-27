bf89ce37be1bec930bab7388ca871a855104f795
me <me@me.me>
2021-06-26 18:30:45 -0700

    a

---
+++ godot_projects/test_rf/physics/ref_frames/physics_env/physics_scene.gd
@@ -0,0 +1,16 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func add_physics_body( body: RigidBody ):
+	add_child( body )
+	body.owner = self
---
+++ godot_projects/test_rf/physics/singletons/root_scene.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+func root_for_bodies():
+	var ret = get_node( "bodies" )
+	return ret
+
+
+func root_for_physics_envs():
+	var ret = get_node( "physics_envs" )
+	return ret
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
