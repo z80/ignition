aadfdd63c9266f972414f5f9ae441b9107963e31
me <me@me.me>
2021-09-11 23:59:20 -0700

    Making an ability to assign a unique name.

--- godot_projects/test_rf/physics/singletons/root_scene.gd
+++ godot_projects/test_rf/physics/singletons/root_scene.gd
@@ -15,6 +15,36 @@ func get_root_for_visuals():
 	return ret
 
 
+func get_unique_name_for_bodies( name_template: String ):
+	var section: Node = get_node( "bodies" )
+	var ret: String = _get_unique_name_for( section, name_template )
+	return ret
+
+
+func get_unique_name_for_physics_envs( name_template: String ):
+	var section: Node = get_node( "physics_envs" )
+	var ret: String = _get_unique_name_for( section, name_template )
+	return ret
+
+
+func get_unique_name_for_visuals( name_template: String ):
+	var section: Node = get_node( "visuals" )
+	var ret: String = _get_unique_name_for( section, name_template )
+	return ret
+
+
+
+
+func _get_unique_name_for( section: Node, name_template: String ):
+	var path: String = section.get_path()
+	var ind: int = 0
+	while true:
+		var name: String = name_template + "_" + str(ind)
+		var p: String = path + "/" + name
+		var n: Node = get_node_or_null( p )
+		if n == null:
+			return name
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
