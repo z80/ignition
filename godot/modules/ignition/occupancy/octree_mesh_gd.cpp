bcbd6924c50e5d7a47281fa0c4a4c6d2ba44110b
me <me@me.me>
2021-10-27 23:02:48 -0700

    Just renamed it.

---
+++ godot/modules/ignition/occupancy/octree_mesh_gd.cpp
@@ -0,0 +1,34 @@
+
+#include "octree_mesh_gd.h"
+
+void OctreeMeshGd::_bind_methods()
+{
+}
+
+OctreeMeshGd::OctreeMeshGd()
+	: Node()
+{
+}
+
+OctreeMeshGd::~OctreeMeshGd()
+{
+}
+
+void OctreeMeshGd::rebuild()
+{
+}
+
+int OctreeMeshGd::faces_qty()
+{
+}
+
+Array OctreeMeshGd::face( int ind )
+{
+}
+
+bool OctreeMeshGd::intersects_ray( const Vector3 & origin, const Vector3 & dir )
+{
+}
+
+
+
---
+++ godot/modules/ignition/occupancy/octree_mesh_gd.h
@@ -0,0 +1,29 @@
+
+#include "scene/main/node.h"
+#include "scene/3d/mesh_instance.h"
+
+#include "octree_mesh.h"
+
+class OctreeMeshGd: public Node
+{
+	GDCLASS(OctreeMeshGd, Node);
+	OBJ_CATEGORY("Ignition");
+
+protected:
+	static void _bind_methods();
+
+public:
+	OctreeMeshGd();
+	~OctreeMeshGd();
+
+	void rebuild();
+	int faces_qty();
+	Array face( int ind );
+	bool intersects_ray( const Vector3 & origin, const Vector3 & dir );
+
+private:
+	OctreeMesh _octree_mesh;
+};
+
+
+
