2a193344c9e9eb3a052db1209e5f4fec85839bff
me <me@me.me>
2022-01-01 22:12:18 -0800

    Fixed atmosphere shader.

--- ignition/physics/camera_ctrl/camera.gd
+++ ignition/physics/camera_ctrl/camera.gd
@@ -495,9 +495,8 @@ func _init_basis():
 
 
 func apply_atmosphere( celestial_body: RefFrameNode ):
-	var planet_radius: float = celestial_body.radius_km * 1000.0
 	var se3: Se3Ref = celestial_body.relative_to( self )
-	var sphere_center: Vector3 = se3.r
+	var planet_center: Vector3 = se3.r
 	
 	# Relative to the Sun.
 	se3 = self.relative_to( null )
@@ -518,6 +517,7 @@ func apply_atmosphere( celestial_body: RefFrameNode ):
 	t.basis = t.basis.scaled( Vector3( far, far, far ) )
 	atm.transform = t
 
+	var planet_radius: float = celestial_body.radius_km * 1000.0
 	var inner_height: float = celestial_body.atmosphere_height_inner_km * 1000.0
 	var outer_height: float = celestial_body.atmosphere_height_outer_km * 1000.0
 	var inner_dist: float   = celestial_body.transparency_dist_inner_km * 1000.0
@@ -525,10 +525,16 @@ func apply_atmosphere( celestial_body: RefFrameNode ):
 	var color_day: Color    = celestial_body.atmosphere_color_day
 	var color_night: Color  = celestial_body.atmosphere_color_night
 	var displacement: float = celestial_body.displacement
+
+
+#	planet_radius += 2.5 * 1000.0
+#	inner_height += 1.0 * 1000.0
+#	outer_height += 1.0 * 1000.0
+#	planet_radius = 1.0
 	
 	var m = atm.get_surface_material( 0 )
-	m.set_shader_param( "sphere_center",               sphere_center )
-	m.set_shader_param( "sphere_radius",               planet_radius )
+	m.set_shader_param( "planet_center",               planet_center )
+	m.set_shader_param( "planet_radius",               planet_radius )
 	m.set_shader_param( "light_dir",                   light_dir )
 	m.set_shader_param( "inner_height",                inner_height )
 	m.set_shader_param( "outer_height",                outer_height )
