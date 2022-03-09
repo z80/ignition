ae2c3aca0bad417fbbe44ed6e0970e9a276b170c
me <me@me.me>
2022-03-08 16:32:21 -0800

    Adjustments...

--- ignition/physics/ui/navigation/navigation.gd
+++ ignition/physics/ui/navigation/navigation.gd
@@ -37,7 +37,7 @@ func _recompute_mode_surface():
 	if not camera:
 		return
 	
-	var ctrl: PhysicsBodyBase = camera.get_parent()
+	var ctrl: PhysicsBodyBase = camera.get_parent() as PhysicsBodyBase
 	if ctrl == null:
 		return
 	var ClosestCelestialBody = preload( "res://physics/utils/closest_celestial_body.gd" )
