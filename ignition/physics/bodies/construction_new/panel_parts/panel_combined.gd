3c042a4ccf26e327dcd45d5350843581c34d2e7c
me <me@me.me>
2022-03-24 23:04:43 -0700

    Making automatic blocks, categories and technologies...

--- ignition/physics/bodies/construction_new/panel_parts/panel_combined.gd
+++ ignition/physics/bodies/construction_new/panel_parts/panel_combined.gd
@@ -1,29 +1,50 @@
 extends Control
 
 signal block_picked( block_desc )
+signal launch
+signal abort
 
 
-func on_category_picked( category: Resource ):
-	var panel_blocks: Panel = get_node( "HBox/PanelBlocks" )
-	panel_blocks.fill_blocks( category )
 
 
-func on_block_picked( block_desc: Resource ):
-	emit_signal( "block_picked", block_desc )
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var panel_categories: Panel = get_node( "HBox/PanelCategories" )
-	panel_categories.connect( "category_picked", self, "on_category_picked" )
+	pass
+	
 
-	var panel_blocks: Panel = get_node( "HBox/PanelBlocks" )
-	panel_blocks.connect( "block_picked", self, "on_block_picked" )
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
+
+
+
+
+func _on_PanelCategories_category_picked( category: Resource ):
+	var panel_blocks: Panel = get_node( "VBox/HBox/PanelBlocks" )
+	panel_blocks.fill_blocks( category )
+
+
+
+func _on_PanelBlocks_block_picked( block_desc: Resource ):
+	emit_signal( "block_picked", block_desc )
+
+
+
+func _on_PanelCommands_launch():
+	emit_signal( "launch" )
+
+
+
+func _on_Control_abort():
+	emit_signal( "abort" )
+
 
 
 
