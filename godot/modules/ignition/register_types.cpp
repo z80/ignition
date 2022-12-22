6c1ffeb5af7c19a3d2918fcc63578032437c4a6d
me <me@me.me>
2022-12-22 15:13:54 -0800

    a

--- godot/modules/ignition/register_types.cpp
+++ godot/modules/ignition/register_types.cpp
@@ -10,6 +10,7 @@
 #include "ref_frame_root.h"
 #include "ref_frame_motion_node.h"
 #include "ref_frame_rotation_node.h"
+#include "ref_frame_non_inertial_node.h"
 #include "ref_frame_auto_node.h"
 #include "se3_ref.h"
 #include "distance_scaler_ref.h"
@@ -54,6 +55,8 @@ void register_ignition_types()
 	ClassDB::register_class<Ign::RefFrameNode>();
 	ClassDB::register_class<Ign::RefFrameRoot>();
 	ClassDB::register_class<Ign::RefFrameMotionNode>();
+	ClassDB::register_class<Ign::RefFrameRotationNode>();
+	ClassDB::register_class<Ign::RefFrameNonInertialNode>();
 	ClassDB::register_class<Ign::RefFrameAutoNode>();
 	ClassDB::register_class<Ign::Se3Ref>();
 	ClassDB::register_class<Ign::DistanceScalerBaseRef>();
