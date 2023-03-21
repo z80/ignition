a95544ff625bce229c3ef1898d84a857a7bf096d
z80 <me@me.me>
2023-03-20 23:28:18 -0700

    Returned sounds back. Accidentally deleted them together with unused models.

--- ignition/physics/parts/thrusters/vacuum/vacuum.gd
+++ ignition/physics/parts/thrusters/vacuum/vacuum.gd
@@ -4,10 +4,10 @@ extends Thruster
 
 
 func init():
-	sound_start = "res://physics/parts/thrusters/thruster_01/assets/start.ogg"
-	sound_loop  = "res://physics/parts/thrusters/thruster_01/assets/loop.ogg"
-	sound_stop  = "res://physics/parts/thrusters/thruster_01/assets/stop.ogg"
-	sound_failed_start = "res://physics/parts/thrusters/thruster_01/assets/crank_no_start.ogg"
+	sound_start = "res://physics/parts/thrusters/assets/start.ogg"
+	sound_loop  = "res://physics/parts/thrusters/assets/loop.ogg"
+	sound_stop  = "res://physics/parts/thrusters/assets/stop.ogg"
+	sound_failed_start = "res://physics/parts/thrusters/assets/crank_no_start.ogg"
 
 	.init()
 
