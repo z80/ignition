48238d5d2ca4aab73cfa5eb06250ae535113bd8e
z80 <z80@z80.z80>
2023-04-02 20:31:11 -0700

    In Godot4 any property assignment calls setter/getter. It causes instant stack overflow.

--- ignition/tryouts/celestial_bodies_voxel_2/surface_source_base.gd
+++ ignition/tryouts/celestial_bodies_voxel_2/surface_source_base.gd
@@ -2,12 +2,18 @@
 extends Resource
 class_name SurfaceSourceBase
 
+var _bounding_radius: float = 1000.0
 @export var bounding_radius: float = 1000.0: get = get_bounding_radius, set = set_bounding_radius
+
+var _source_radius: float = 800.0
 @export var source_radius: float = 800.0: get = get_source_radius, set = set_source_radius
 @export var radius_km: float: get = get_radius_km, set = set_radius_km
-@export var orbiting_velocity: float: get = get_orbiting_velocity, set = set_orbiting_velocity
-@export var g: float: get = get_g, set = set_g
-#export(float) var gm setget , get_gm
+
+var _orbiting_velocity: float = 100.0
+@export var orbiting_velocity: float = 100.0: get = get_orbiting_velocity, set = set_orbiting_velocity
+
+var _g: float = 9.8
+@export var g: float 9.8: get = get_g, set = set_g
 
 @export var focus_depth: float   = 40.0
 
@@ -31,46 +37,46 @@ func _set_drop_materials_liquid_here( m: Material ):
 
 
 func set_bounding_radius( r: float ):
-	bounding_radius = r
+	_bounding_radius = r
 
 
 func get_bounding_radius():
-	return bounding_radius
+	return _bounding_radius
 
 
 func set_source_radius( v: float ):
-	source_radius = v
+	_source_radius = v
 	notify_property_list_changed()
 
 
 func get_source_radius():
-	return source_radius
+	return _source_radius
 
 
 func set_radius_km( r_km: float ):
-	source_radius = r_km * 1000.0
+	_source_radius = r_km * 1000.0
 	notify_property_list_changed()
 
 
 func get_radius_km():
-	var ret: float = source_radius * 0.001
+	var ret: float = _source_radius * 0.001
 	return ret
 
 
 func set_orbiting_velocity( v: float ):
-	orbiting_velocity = v
-	g = v*v/source_radius
-	notify_property_list_changed()
+	_orbiting_velocity = v
+	_g = v*v/_source_radius
+	#notify_property_list_changed()
 
 
 func get_orbiting_velocity():
-	return orbiting_velocity
+	return _orbiting_velocity
 
 
 func set_g( gg: float ):
-	g = gg
-	orbiting_velocity = sqrt(g*source_radius)
-	notify_property_list_changed()
+	_g = gg
+	_orbiting_velocity = sqrt(_g*_source_radius)
+	#notify_property_list_changed()
 
 
 func get_g():
@@ -78,7 +84,7 @@ func get_g():
 
 
 func get_gm():
-	var gm: float = orbiting_velocity * orbiting_velocity * source_radius
+	var gm: float = _orbiting_velocity * _orbiting_velocity * _source_radius
 	return gm
 
 func get_source_solid():
