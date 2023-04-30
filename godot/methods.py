df545a82e8532a7e07e903b4cf93d392e41621c7
z80 <z80@z80.z80>
2023-04-29 23:53:14 -0700

    Changed the displayed version.

--- godot/methods.py
+++ godot/methods.py
@@ -70,7 +70,8 @@ def add_module_version_string(self, s):
 
 def get_version_info(module_version_string="", silent=False):
     #build_name = "custom_build"
-    build_name = "commit_7aca0f70"
+    #build_name = "commit_7aca0f70"
+    build_name = "commit_b49f801f3"
     if os.getenv("BUILD_NAME") != None:
         build_name = str(os.getenv("BUILD_NAME"))
         if not silent:
