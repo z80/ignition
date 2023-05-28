561502993c2ddaffb6a52db5f1e8d99aa01e5bbf
z80 <z80@z80.z80>
2023-05-28 16:50:10 -0700

    Here it still works no problem.

--- stuff/godot_projects/test_viwport_texture/scenes/main.gd
+++ stuff/godot_projects/test_viwport_texture/scenes/main.gd
@@ -3,13 +3,15 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#return
 	var viewport: SubViewport = get_node( "SubViewport" )
 	var plane: MeshInstance3D = get_node( "Plane" )
-	var material: StandardMaterial3D = plane.get_surface_override_material(0)
+	var material: StandardMaterial3D = plane.get_surface_override_material(0).duplicate()
 	var tex: ViewportTexture = material.albedo_texture
 	var path: NodePath = viewport.get_path()
 	tex.viewport_path = path
 	material.albedo_texture = tex
+	plane.set_surface_override_material( 0, material )
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
